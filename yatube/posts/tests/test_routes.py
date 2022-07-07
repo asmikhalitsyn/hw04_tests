a51dcc0c18766f281942f7456e030abe42092ac9
Andrey Mikhalitsyn <andreymikh92@gmail.com>
2022-07-07 12:46:45 +0300

    Исправил замечания 5

--- yatube/posts/tests/test_routes.py
+++ yatube/posts/tests/test_routes.py
@@ -17,12 +17,18 @@ class PostRoutesTests(TestCase):
         self.NAME_OF_URL_TO_EDIT_POST = 'post_edit'
         cases = [
             [NAME_OF_URL_OF_INDEX, '/', None],
-            [NAME_OF_URL_OF_POSTS_OF_GROUP, f'/group/{SLUG_OF_GROUP}/', [SLUG_OF_GROUP]],
+            [NAME_OF_URL_OF_POSTS_OF_GROUP,
+             f'/group/{SLUG_OF_GROUP}/',
+             [SLUG_OF_GROUP]],
             [NAME_URL_OF_PROFILE, f'/profile/{USERNAME}/', [USERNAME]],
             [self.NAME_OF_URL_OF_DETAIL_POST, f'/posts/{POST_ID}/', [POST_ID]],
-            [self.NAME_OF_URL_TO_EDIT_POST, f'/posts/{POST_ID}/edit/', [POST_ID]],
+            [self.NAME_OF_URL_TO_EDIT_POST,
+             f'/posts/{POST_ID}/edit/',
+             [POST_ID]],
             [NAME_URL_TO_CREATE_POST, '/create/', None]
         ]
         for name_of_route, route, args in cases:
             with self.subTest(name_of_route=name_of_route):
-                self.assertEqual(reverse(f'posts:{name_of_route}', args=args), route)
+                self.assertEqual(
+                    reverse(f'posts:{name_of_route}', args=args), route
+                )
